ec600f2e7b228dac3168d010291fcb05cc48678b
HunterZ1994 <Martin.Thuerauf.1994@gmail.com>
2017-05-11 17:08:13 +0200

    commited on 11.03.2017

---
+++ Four-the-win/src/de/hsMannheim/informatik/oot/ss17/ttt/view/test.java
@@ -0,0 +1,9 @@
+package de.hsMannheim.informatik.oot.ss17.ttt.view;
+
+public class test {
+	public static void main(String[] args) {
+		int[] size = {15,15};
+		UserInterface UI = new UserInterface(size);
+		UI.print();
+	}
+}
