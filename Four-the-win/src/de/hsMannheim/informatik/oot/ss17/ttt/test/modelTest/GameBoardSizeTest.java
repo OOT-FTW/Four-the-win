de208b5156981244de0e7ce19c4c69816bc5ad11
Sebastian J. Vogt <1620115@stud.hs-mannheim.de>
2017-05-18 10:19:10 +0200

    Test Klassen erstellt. Sollten heute implementiert werden.

---
+++ Four-the-win/src/de/hsMannheim/informatik/oot/ss17/ttt/test/modelTest/GameBoardSizeTest.java
@@ -0,0 +1,34 @@
+package de.hsMannheim.informatik.oot.ss17.ttt.test.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameBoardSizeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGameBoardSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColumns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRows() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Four-the-win/src/de/hsMannheim/informatik/oot/ss17/ttt/test/modelTest/GameBoardTest.java
@@ -0,0 +1,74 @@
+package de.hsMannheim.informatik.oot.ss17.ttt.test.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameBoardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGameBoardGameBoardSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGameBoardString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertToken() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTokenAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRows() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColumns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentPlayerNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsAnyNonNoneToken() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+}
